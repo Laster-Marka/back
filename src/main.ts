d6004ec8a7c777139dc888a1205a11476a65b0a1
iruizdorronsoro <iruizdorronsoro@gmail.com>
2021-10-09 01:04:01 +0200

    cookie tut 3

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true
   })
-  app.use(cookieParser())
   app.enableCors({
     credentials: true,
-    origin: true
+    origin: 'https://laster-marka.herokuapp.com'
   })
+  app.use(cookieParser())
   await app.listen(process.env.PORT || 4000)
 }
 bootstrap()
